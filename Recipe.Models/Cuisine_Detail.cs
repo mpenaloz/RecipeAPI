f670cc5f99bc55eb3f2becacfa4cd1cf3d838ec6
alexlewis1990 <alexlewis199015@gmail.com>
2021-09-21 11:03:11 -0400

    first commit

---
+++ Recipe.Models/Cuisine_Detail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Models
+{
+   public class Cuisine_Detail
+    {
+        public int Id { get; set; }
+        public string CuisineName { get; set; }
+    }
+}
---
+++ Recipe.Models/Cuisine_update.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Models
+{
+    public class Cuisine_update
+    {
+        public int Cuisineid { get; set; }
+        public string CuisineName { get; set; }
+    }
+}
